179b9b48037a06f0ef6d46d7a715a0a8927e559b
SotiriaMpont <80161152+SotiriaMpont@users.noreply.github.com>
2023-05-12 16:58:59 +0300

    Add files via upload

---
+++ form_broker.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Broker Page</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <nav>
+    <ul>
+      <li><a href="#rate">Rate a Distributor</a></li>
+      <li><a href="#scores">View Distributor Scores</a></li>
+      <li><a href="#weights">Adjust Metric Weights</a></li>
+      <li><a href="#logout">Log Out</a></li>
+    </ul>
+  </nav>
+  <main>
+    <section id="rate">
+      <!-- Rate a Distributor form goes here -->
+    </section>
+    <section id="scores">
+      <!-- Distributor Scores table goes here -->
+    </section>
+    <section id="weights">
+      <!-- Metric Weights form goes here -->
+    </section>
+    <section id="logout">
+      <p>Are you sure you want to log out?</p>
+      <button>Log Out</button>
+    </section>
+  </main>
+</body>
+</html>
