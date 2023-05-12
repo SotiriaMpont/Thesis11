b03584b10fb176cf9b1c981f12dc5c9130b83746
SotiriaMpont <80161152+SotiriaMpont@users.noreply.github.com>
2023-05-12 16:49:27 +0300

    Add files via upload

---
+++ broker_dashboard.php
@@ -0,0 +1,56 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "123456789";
+$dbname = "diplomatiki11";
+
+// Start the session and get the distributor ID
+
+$id_distributor = $_GET['id'];
+
+// Get the shift date from the GET parameters
+if (isset($_GET['date'])) {
+    $date = $_GET['date'];
+} else {
+    error_log("date is not set in the GET parameters");
+    exit;
+}
+
+error_log("date: $date, distributor_id: $id_distributor");
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Execute the SQL query
+$sql = "SELECT * FROM Total_Score WHERE shift = '$date' AND distributor_id='$id_distributor'";
+
+$result = mysqli_query($conn, $sql);
+
+if ($result !== false) {
+    // Get the total score value
+    $row = mysqli_fetch_assoc($result);
+    if ($row !== null) {
+        $total_score = $row['total_rate'];
+        $total_rate_costumer = $row['total_rate_costumer'];
+        $total_rate_store = $row['total_rate_store'];
+        $total_rate_broker = $row['total_rate_broker'];
+        $total_rate_metrics = $row['metrics_rate'];
+
+        include 'distributor.html';
+        error_log("Total rate metrics: $total_rate_metrics");
+    } else {
+        error_log("No rows returned from the query");
+    }
+} else {
+    error_log("Error executing query: " . mysqli_error($conn));
+}
+
+// Close the database connection
+mysqli_close($conn);
+
+?>
