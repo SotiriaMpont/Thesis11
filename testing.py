c157295a1304ad9eb212a876f142a371d88f16e2
SotiriaMpont <80161152+SotiriaMpont@users.noreply.github.com>
2023-05-12 17:06:42 +0300

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,26 @@
+import script 
+
+#to xrisimopoiw gia thn arxikopoisi
+#for i in range(8): 
+#script.InsertDistributor_toDatabase()
+# Καταχωρώ τυχαία αιτήματα στην βάση δεδομένων με Locations για το store, locations για τον costumer και την απόσταση τους σε οδικό δίκτυο
+#for i in range(6):
+    #script.Kataxwrise_Aitima_database()
+
+#Δηλώνει για όλους τους διανομείς την μέρα που έχω ως βάρδια και δηλώνει επίσης μια τυχαία ωρα που ξεκινα και τελειώνει 
+#script.Dilwsi_Shift()
+
+#Για την πρώτη φορά που αναθέτω αιτήματα στους διανομεις γιατι μετα θα παει με βάση την μετρική, βρίσκει και το acceptance rate για το shift της σημερινής μέρας
+
+#script.Apodoxi_Aitimatos_Distributor()
+#for i in range(20):
+ #script.Actual_km()
+
+script.Rating_From_Store()
+
+script.Rating_From_Broker()
+
+script.Rating_From_Costumer()
+
+#script.metrics()
+
